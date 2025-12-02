03f91f5b8f4753ca799643d0b6466da4e1a173cf
Eryk Lepszy <eryklepszy@protonmail.com>
2025-12-02 10:53:09 +0100

    Add initial day 2 solutions

---
+++ src/day2.rs
@@ -0,0 +1,80 @@
+use std::collections::HashSet;
+use std::ops::RangeInclusive;
+
+pub fn solve_part1(input: &str) -> u64 {
+    let ranges = parse_input(input);
+    ranges.map(|range| add_invalid_ids(range)).sum()
+}
+
+pub fn solve_part2(input: &str) -> u64 {
+    let ranges = parse_input(input);
+    ranges
+        .map(|range| add_invalid_ids_with_repeats(range))
+        .sum()
+}
+
+fn parse_input(input: &str) -> impl Iterator<Item=RangeInclusive<u64>> {
+    input
+        .lines()
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|range| range.split_once('-').unwrap())
+        .map(|(start, end)| start.parse::<u64>().unwrap()..=end.parse::<u64>().unwrap())
+}
+
+fn add_invalid_ids(range: RangeInclusive<u64>) -> u64 {
+    let mut invalid_ids_value = 0u64;
+    range.for_each(|number| {
+        let digits = number_to_digits(number);
+        if digits.len() % 2 == 0 {
+            let mid_index = digits.len() / 2;
+            let (first, second) = digits.split_at(mid_index);
+            if first == second {
+                invalid_ids_value += number;
+            };
+        }
+    });
+    invalid_ids_value
+}
+
+fn add_invalid_ids_with_repeats(range: RangeInclusive<u64>) -> u64 {
+    let mut invalid_ids = HashSet::new();
+    range.for_each(|number| {
+        let digits = number_to_digits(number);
+        let max_split = digits.len() / 2;
+        (1..=max_split).for_each(|split| {
+            if digits.len() % split == 0 {
+                let mut chunks = digits.chunks_exact(split);
+                let pattern = chunks.next().unwrap();
+                if chunks.all(|chunk| chunk == pattern) {
+                    invalid_ids.insert(number);
+                };
+            }
+        })
+    });
+    invalid_ids.iter().sum()
+}
+
+fn number_to_digits(number: u64) -> Vec<u64> {
+    let mut number = number;
+    let mut digits = vec![];
+    while number > 0 {
+        digits.push(number % 10);
+        number = number / 10;
+    }
+    digits.reverse();
+    digits
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn splits_number_to_digits() {
+        let number = 123456789u64;
+        let digits = number_to_digits(number);
+        assert_eq!(digits, vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod day1;
+mod day2;
 
 use std::fs;
 use std::path::Path;
@@ -9,6 +10,12 @@ fn main() {
     println!("Day 1, part 1 solution: {}", solution1_1);
     let solution1_2 = day1::solve_part2(&day1_input);
     println!("Day 1, part 2 solution: {}", solution1_2);
+
+    let day2_input = read_input(Path::new("inputs/day2.txt"));
+    let solution2_1 = day2::solve_part1(&day2_input);
+    println!("Day 2, part 1 solution: {}", solution2_1);
+    let solution2_2 = day2::solve_part2(&day2_input);
+    println!("Day 2, part 2 solution: {}", solution2_2);
 }
 
 fn read_input(path: &Path) -> String {
@@ -31,5 +38,19 @@ mod tests {
         let day1_input = read_input(Path::new("inputs/day1.txt"));
         let solution = day1::solve_part2(&day1_input);
         assert_eq!(solution, 5657);
+    }
+
+    #[test]
+    fn day2_part1_returns_correct_solution() {
+        let day2_input = read_input(Path::new("inputs/day2.txt"));
+        let solution = day2::solve_part1(&day2_input);
+        assert_eq!(solution, 31000881061);
+    }
+
+    #[test]
+    fn day2_part2_returns_correct_solution() {
+        let day2_input = read_input(Path::new("inputs/day2.txt"));
+        let solution = day2::solve_part2(&day2_input);
+        assert_eq!(solution, 46769308485);
     }
 }
